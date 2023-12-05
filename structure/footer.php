c689e1e3a506d38c556e3b4131c3e99d48584f46
aredondop <102090402+aredondop@users.noreply.github.com>
2023-12-05 15:15:04 +0100

    Subida inicial de la Tarea

---
+++ structure/footer.php
@@ -0,0 +1,10 @@
+<footer>
+  <div class="row">
+    <div class="col-lg-6 ">
+      <small>&copy; Ángel Redondo Pliego <?php echo date('Y'); ?></small>
+    </div>
+    <div class="col-lg-6" style="text-align:right;">
+      <small>Hecho con ❤️ para IAW</small>
+    </div>
+  </div>
+</footer>
---
+++ structure/menu.php
@@ -0,0 +1,13 @@
+<div class="container">
+  <a href="index.php" class="navbar-brand">Mi Página Web Personal</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarResponsive">
+    <ul class="navbar-nav">
+      <li class="nav-item"><a class="nav-link" href="operacion.php">Operación</a></li>
+      <li class="nav-item"><a class="nav-link" href="datos.php">Datos</a></li>
+      <li class="nav-item"><a class="nav-link" href="sesion.php">Sesión</a></li>
+    </ul>
+  </div>
+</div>
